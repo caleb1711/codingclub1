d9a0c314a38db16db7e840625ea0ce44864b2a4a
caleb1711 <>
2023-06-16 08:03:48 +0000

    style the signup, login forms

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
-console.log("yes");
+const formOpenBtn = document.querySelector("#form-open");
+home = document.querySelector(".home");
+formContainer = document.querySelector(".form_container");
+formCloseBtn = document.querySelector(".form_close");
+signupBtn = document.querySelector("#signup");
+loginBtn = document.querySelector("#signup");
+pwShowHide = document.querySelector("#signup");
