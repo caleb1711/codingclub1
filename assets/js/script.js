9becec139a39971df2291b2a752e00f0138bcb4a
caleb1711 <>
2023-06-16 13:04:06 +0000

    style and add js query selector, eventlistener to the login and signup forms

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,23 @@
-const formOpenBtn = document.querySelector("#form-open");
-home = document.querySelector(".home");
-formContainer = document.querySelector(".form_container");
-formCloseBtn = document.querySelector(".form_close");
-signupBtn = document.querySelector("#signup");
-loginBtn = document.querySelector("#signup");
-pwShowHide = document.querySelector("#signup");
+const formOpenBtn = document.querySelector("#form-open"),
+    home = document.querySelector(".home"),
+    formContainer = document.querySelector(".form_container"),
+    formCloseBtn = document.querySelector(".form_close"),
+    signupBtn = document.querySelector("#signup"),
+    loginBtn = document.querySelector("#login"),
+    pwShowHide = document.querySelectorAll(".pw_hide");
+
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+signupBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.add("active");
+
+});
+loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.remove("active");
+
+});
+
